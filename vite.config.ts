e156edf4f90a20b869cb9887146c07c2ca509d30
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-12-27 17:59:40 +0100

    fix: enable strict file system checks in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: false, // Deaktiviert Hot Module Replacement (HMR)
+    hmr: false,
+     fs:{
+      strict: true
+     }
   }
 })
 
